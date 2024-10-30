8b72f55138173c31592efe27203a6b3275c16c99
saulopm <saulopmarinho@terra.com.br>
2024-10-30 17:21:42 -0300

    first commit

---
+++ src/Contatos/Formulario/LIstaContato/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ContatosList } from '../../../Contatos'
+import { Formulario } from '../../Formulario'
+
+const ListaContato = () => (
+  <>
+    <Formulario />
+    <ContatosList />
+  </>
+)
+export default ListaContato
---
+++ src/Contatos/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const ContainerContatos = styled.div`
+  width: 100%;
+  margin-top: 60px;
+  background-color: #dfe6e9;
+  border-radius: 4px;
+`
+
+export const ContainerContatoIndividual = styled.div`
+  margin: 0 auto;
+  padding: 20px;
+  max-width: 900px;
+  background-color: #2d3436;
+  border-radius: 8px;
+`
+
+export const Title = styled.h3`
+  font-size: 30px;
+  color: #2d3436;
+  text-align: center;
+  padding-top: 30px;
+`
+
+export const Text = styled.h5`
+  font-size: 15px;
+  color: #dfe6e9;
+`
+
+export const Button = styled.button`
+  border-radius: 50px;
+  background-color: #dfe6e9;
+  color: #2d3436;
+  height: 30px;
+  margin: 0 auto;
+  font-weight: bold;
+  width: 100px;
+  transition: background-color 0.3s;
+  margin-right: 20px;
+  &:hover {
+    background-color: #636e72;
+  }
+`
